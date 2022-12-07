fcfdc0535c5ce7a80edb616592acb05ce41873f9
XenithMusic <36333004+XenithMusic@users.noreply.github.com>
2022-12-07 12:08:53 -0500

    Update game.py

--- gm-library/1/game.py
+++ gm-library/1/game.py
@@ -1,1 +1,1 @@
-{"name":"test","description":"Testing game.","price":1.99,"latest-downloads":"downloads/$","latest-version":"1.0"}
+{"name":"test","description":"Testing game.","price":1.99,"latest-downloads":"downloads-$","latest-version":"1.0"}
